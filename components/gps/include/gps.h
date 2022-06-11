35eb8e22de178baa61f1cff6132489b8c30e5488
Mrnikifabio <3849203-Mrnikifabio@users.noreply.gitlab.com>
2022-06-11 13:42:17 +0200

    Added .gitignore GPS Module added NMEA Parsing Library added

---
+++ components/gps/include/gps.h
@@ -0,0 +1,33 @@
+#ifndef GPS_H
+#define GPS_H
+
+#include <string.h>
+#include <stdlib.h>
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "nmea.h"
+#include "gpgll.h"
+#include "gpgga.h"
+#include "gprmc.h"
+#include "gpgsa.h"
+#include "gpvtg.h"
+#include "gptxt.h"
+#include "gpgsv.h"
+
+
+typedef struct {
+    double latitude;
+    double longitude;
+} GPSModuleCoordinates;
+
+
+static const uart_port_t uart_num = UART_NUM_1;
+static const int uart_buffer_size = (1024);
+static const int U2_TX = 34;
+static const int U2_RX = 35;
+
+void setup_gps();
+GPSModuleCoordinates* read_gps_coordinates();
+
+
+#endif //GPS_H
