91e6a66d14fec32d2f26b4561c8c407d219ed2f6
MartBent <martbent99@gmail.com>
2022-06-22 21:15:55 +0200

    Finally removed unusabled components

--- components/gps/include/gps.h
+++ components/gps/include/gps.h
@@ -13,8 +13,6 @@ typedef struct {
 
 static const uart_port_t gps_uart_num = UART_NUM_1;
 static const int uart_buffer_size = (1024);
-static const int U2_TX = 0;
-static const int U2_RX = 4;
 
 void setup_gps();
 void read_gps_coordinates(GPSModuleCoordinates* coordinates);
